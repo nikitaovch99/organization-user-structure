da26d06e6f6d77148827400cf8dbdc14e154c064
Nikita Ovchinnikov <nikitaovch99@gmail.com>
2023-02-08 17:03:20 +0200

    changed port

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(process.env.SERVER_PORT || 3000, () => {
+  console.log(`Server started on port ${process.env.SERVER_PORT}`);
 });
